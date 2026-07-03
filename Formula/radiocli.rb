85b385b758fae6e4d942e7a2ae9f5a9e69cc6759
Ciphore <neal@ciphore.io>
2026-07-02 20:49:48 -0700

    Update radiocli to 0.1.8

--- Formula/radiocli.rb
+++ Formula/radiocli.rb
@@ -1,8 +1,8 @@
 class Radiocli < Formula
   desc "Terminal-first world radio receiver"
   homepage "https://github.com/Ciphore/RadioCLI"
-  url "https://registry.npmjs.org/@ciphore/radiocli/-/radiocli-0.1.7.tgz"
-  sha256 "85b959cbc842789fdf1e8c5b16cc3b707d5cb7e20d10db4c727dd8cd59049831"
+  url "https://registry.npmjs.org/@ciphore/radiocli/-/radiocli-0.1.8.tgz"
+  sha256 "4f4cfa0040b3d7582d62f1ac909db0cda65952da4fef6f92b47bc80db0802568"
   license "MIT"
 
   depends_on "ffmpeg"
